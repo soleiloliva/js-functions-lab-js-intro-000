7d6833ea31567af5368e90f8ff2c26167b26582d
Soleil Oliva <soleiloliva@gmail.com>
2020-06-22 21:37:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 
 }
-function happyCustomHolidayTo(holiday + name) {
-  return (`Happy holidays, ${holiday},  ${name}!`)
-}
+
+
